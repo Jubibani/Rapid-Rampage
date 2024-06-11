7930d9190ce5835a454beb08165113ca2db7a002
Jubibani <strawberryloli3@gmail.com>
2024-06-12 02:38:42 +0800

    clamping the move speed
    - move speed clamped between 0 to max speed.
    
    more to be followed...

--- player.gd
+++ player.gd
@@ -8,8 +8,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var move_speed = 1000.0
-	var jump_strength = 1000.0
+	var move_speed := 1000.0
+	var max_speed := 10.0
+	var jump_strength := 1000.0
 	var force := Vector3.ZERO
 	
 	if Input.is_action_pressed("forward"):
@@ -24,6 +25,18 @@ func _process(delta):
 	if Input.is_action_pressed("jump"):
 		force += transform.basis.y * jump_strength * delta
 	apply_central_force(force)
+	
+	#clamping
+	move_speed = linear_velocity.length()
+	move_speed = clamp(move_speed,0,max_speed)
+	
+	#limit
+	if linear_velocity.length() > max_speed:
+		linear_velocity = linear_velocity.normalized() * max_speed
+	
+	
+	print("move speed: ", move_speed)
+	print("max speed: ", max_speed)
 
 func _input(event):
 	if event is InputEventScreenDrag:
