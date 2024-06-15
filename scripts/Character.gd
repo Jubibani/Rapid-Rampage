862b59609868786ddf505bc2a2a55c814cfbe133
Jubibani <strawberryloli3@gmail.com>
2024-06-15 14:28:09 +0800

    added Anchor on UI
    Fixes:
    - Implemented Anchor on UI player control
    - fixed double jump when in air
    - fixed less ugly button
    
    more to be followed...

--- scripts/Character.gd
+++ scripts/Character.gd
@@ -1,13 +1,24 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+var SPEED = walking_speed
+const walking_speed = 5.5
+const running_speed = 12.5
+var running : bool = false
 const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
+ 
 func _input(event):
+	if Input.is_action_just_released("run") and is_on_floor():
+		running = not running   
+		if running:
+			SPEED = running_speed
+			print("running")
+		else:
+			SPEED = walking_speed
+		
 	if event is InputEventScreenDrag:
 		var look_sensitivity = 0.5
 		rotate_y(deg_to_rad(-event.relative.x * look_sensitivity))
@@ -39,3 +50,23 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+	print("speed: ", SPEED)
+
+
+
+func _on_jump_pressed():
+	# Handle jump.
+	if is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		print("jumped")
+
+
+func _on_run_pressed():
+	running = not running   
+	if running:
+		SPEED = running_speed
+		print("running")
+	else:
+		SPEED = walking_speed
+	
+	
