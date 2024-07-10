82435da4046a96cd97cf242b4867fe9e5346a957
Jubibani <strawberryloli3@gmail.com>
2024-07-11 04:39:29 +0800

    still learning FSM
    Status: Stuck/ Learning/ WIP
    
    How to implement?
    
    more to be followed...

--- scripts/Character.gd
+++ scripts/Character.gd
@@ -1,18 +1,10 @@
 extends CharacterBody3D
 class_name CharacterPlayer
 
-@export var state_machine : StateMachine
 @export var animation_player: AnimationPlayer
 @export_range(5.0, 10.0) var animation_speed = 5.0
 
-enum State {STANDING, CROUCHING, PRONE}
-var current_state = State.STANDING
-
-var SPEED = walking_speed
-
-var is_running : bool = false
 var is_crouching : bool = false
-var is_proning : bool = false
 
 const proning_speed = 1.8
 const crouching_speed = 2.3
@@ -20,15 +12,13 @@ const walking_speed = 5.5
 const running_speed = 12.5
 const JUMP_VELOCITY = 4.5
 
+var SPEED = walking_speed
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-func _unhandled_input(event):
-	if Input.is_action_pressed("crouch"):
-		state_machine.handle_input(event)
-
+func _ready():
+	pass
 func _input(event):
-	#if Input.is_action_just_pressed("crouch") and is_on_floor():
-		#state_machine
+	if Input.is_action_just_pressed("crouch") and is_on_floor():
 		#match current_state:
 			#State.STANDING:
 				#_crouch()
@@ -36,22 +26,23 @@ func _input(event):
 				#_prone() # Changed from _uncrouch()
 			#State.PRONE:
 				#_uncrouch() # Added
-
+		pass
 	if Input.is_action_just_pressed("prone") and is_on_floor():
-		match current_state:
-			State.STANDING:
-				_prone()
-			State.CROUCHING:
-				_uncrouch() # Added
-			State.PRONE:
-				_crouch() # Added
+		#match current_state:
+			#State.STANDING:
+				#_prone()
+			#State.CROUCHING:
+				#_uncrouch() # Added
+			#State.PRONE:
+				#_crouch() # Added
+		pass
 
 	if Input.is_action_just_pressed("run") and is_on_floor():
-		if current_state == State.STANDING:
-			SPEED = running_speed
-		else:
-			SPEED = walking_speed
-
+		#if current_state == State.STANDING:
+			#SPEED = running_speed
+		#else:
+			#SPEED = walking_speed
+		pass
 	if event is InputEventScreenDrag:
 		var look_sensitivity = 0.5
 		rotate_y(deg_to_rad(-event.relative.x * look_sensitivity))
@@ -63,7 +54,7 @@ func _input(event):
 		$head.rotation_degrees = head_rotation
 
 func _physics_process(delta):
-	state_machine.update(delta)
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -72,14 +63,6 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Adjust movement speed based on current state
-	match current_state:
-		State.STANDING:
-			SPEED = walking_speed
-		State.CROUCHING:
-			SPEED = crouching_speed
-		State.PRONE:
-			SPEED = proning_speed
 
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
@@ -94,73 +77,61 @@ func _physics_process(delta):
 	move_and_slide()
 
 func _crouch():
-	if current_state == State.STANDING:
-		current_state = State.CROUCHING
-		animation_player.play("CROUCHING", -1, animation_speed)
-	elif current_state == State.PRONE: # Added
-		current_state = State.CROUCHING
-		animation_player.play("ProneToStand", -1, animation_speed) # Changed animation name
-		# Adjust collider size and position
+	
+	#if current_state == State.STANDING:
+		#current_state = State.CROUCHING
+		#animation_player.play("CROUCHING", -1, animation_speed)
+	#elif current_state == State.PRONE: # Added
+		#current_state = State.CROUCHING
+		#animation_player.play("ProneToStand", -1, animation_speed) # Changed animation name
+		## Adjust collider size and position
 
-func _uncrouch():
-	if current_state == State.CROUCHING:
-		current_state = State.STANDING
-		animation_player.play("CROUCHING", -1, -animation_speed, true)
-	elif current_state == State.PRONE: # Added
-		current_state = State.STANDING
-		animation_player.play("ProneToStand", -1, -animation_speed, true) # Changed animation name
-		# Restore collider size and position
+#func _uncrouch():
+	#if current_state == State.CROUCHING:
+		#current_state = State.STANDING
+		#animation_player.play("CROUCHING", -1, -animation_speed, true)
+	#elif current_state == State.PRONE: # Added
+		#current_state = State.STANDING
+		#animation_player.play("ProneToStand", -1, -animation_speed, true) # Changed animation name
+		## Restore collider size and position
+#
+#func _prone():
+	#if current_state == State.STANDING:
+		#current_state = State.PRONE
+		#animation_player.play("StandProne", -1, animation_speed)
+	#elif current_state == State.CROUCHING: # Added
+		#current_state = State.PRONE
+		#animation_player.play("CrouchProne", -1, animation_speed) # Changed animation name
+		## Adjust collider size and position
+#
+#func _unprone():
+	#if current_state == State.PRONE:
+		#current_state = State.STANDING
+		#animation_player.play("StandProne", -1, -animation_speed, true)
+	#elif current_state == State.CROUCHING: # Added
+		#current_state = State.STANDING
+		#animation_player.play("CrouchProne", -1, -animation_speed, true) # Changed animation name
 
-func _prone():
-	if current_state == State.STANDING:
-		current_state = State.PRONE
-		animation_player.play("StandProne", -1, animation_speed)
-	elif current_state == State.CROUCHING: # Added
-		current_state = State.PRONE
-		animation_player.play("CrouchProne", -1, animation_speed) # Changed animation name
-		# Adjust collider size and position
-
-func _unprone():
-	if current_state == State.PRONE:
-		current_state = State.STANDING
-		animation_player.play("StandProne", -1, -animation_speed, true)
-	elif current_state == State.CROUCHING: # Added
-		current_state = State.STANDING
-		animation_player.play("CrouchProne", -1, -animation_speed, true) # Changed animation name
-
-func _run():
-	if !is_crouching:
-		if !is_running:
-			SPEED = running_speed
-			print("running at the speed of: ", SPEED)
-		elif is_crouching:
-			SPEED = crouching_speed
-		elif is_running:
-			SPEED = walking_speed
-			print("walking at the speed of: ", SPEED)
-	elif SPEED == walking_speed:
-		SPEED = running_speed
-		print("running at the speed of: ", SPEED)	
-	else:
-		SPEED = walking_speed
-		print("from run(), and walking at the speed of: ", SPEED)	
-
+#func _run():
+	#if !is_crouching:
+		#if !is_running:
+			#SPEED = running_speed
+			#print("running at the speed of: ", SPEED)
+		#elif is_crouching:
+			#SPEED = crouching_speed
+		#elif is_running:
+			#SPEED = walking_speed
+			#print("walking at the speed of: ", SPEED)
+	#elif SPEED == walking_speed:
+		#SPEED = running_speed
+		#print("running at the speed of: ", SPEED)	
+	#else:
+		#SPEED = walking_speed
+		#print("from run(), and walking at the speed of: ", SPEED)	
+	pass
 
 func _on_jump_pressed():
 	# Handle jump.
 	if is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		print("jumped")
-
-
-func _on_run_pressed():
-	if !is_crouching and is_on_floor():
-		_run()
-	is_running = !is_running
-
-func _on_crouch_pressed():
-	if !is_crouching:
-		_crouch()
-	else:
-		_uncrouch()
-	is_crouching = !is_crouching
--- scripts/State.gd
+++ scripts/State.gd
@@ -1,19 +1,37 @@
-extends Node
+# Virtual base class for all states.
 class_name State
+extends Node
 
-var state_machine : StateMachine
+# Reference to the state machine, to call its `transition_to()` method directly.
+# That's one unorthodox detail of our state implementation, as it adds a dependency between the
+# state and the state machine objects, but we found it to be most efficient for our needs.
+# The state machine node will set it.
+var state_machine = null
 
-func enter():
+
+# Virtual function. Receives events from the `_unhandled_input()` callback.
+func handle_input(_event: InputEvent) -> void:
+	##what does it do tho?
 	pass
 
-func exit():
+
+# Virtual function. Corresponds to the `_process()` callback.
+func update(_delta: float) -> void:
 	pass
 
-func handle_input(_event: InputEvent):
+
+# Virtual function. Corresponds to the `_physics_process()` callback.
+func physics_update(_delta: float) -> void:
 	pass
-	
-func update(event):
+
+
+# Virtual function. Called by the state machine upon changing the active state. The `msg` parameter
+# is a dictionary with arbitrary data the state can use to initialize itself.
+func enter(_msg := {}) -> void:
 	pass
-	
-func get_player() -> CharacterBody3D:
-	return state_machine.get_player()
+
+
+# Virtual function. Called by the state machine before changing the active state. Use this function
+# to clean up the state.
+func exit() -> void:
+	pass
