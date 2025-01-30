40805eb1da161a4dfbdbac009a4852391c7cae07
Jubibani <strawberryloli3@gmail.com>
2025-01-31 02:34:09 +0800

    fix jump state after wall bounce

--- states/wall_bounce.gd
+++ states/wall_bounce.gd
@@ -3,10 +3,12 @@ extends PlayerState
 
 func enter(previous_state_path: String, data := {}) -> void:
 		print("wallbounce")
+		CharacterPlayer.is_wall_bouncing = true
 		wall_bounce()
 
 
 func physics_update(_delta: float) -> void:
+	CharacterPlayer.is_wall_bouncing = false
 	finished.emit(Standing)
 			
 	#! this is to release these pressed buttons when pressed midair
@@ -17,17 +19,17 @@ func physics_update(_delta: float) -> void:
 
 #feature movement
 func wall_bounce():
-	#if CharacterPlayer.is_on_wall():
-	CharacterPlayer.last_wall_normal = CharacterPlayer.get_wall_normal()  # Get direction of the wall
-	print("Wall Bounce Normal: ", CharacterPlayer.last_wall_normal)  # Debugging
+	if CharacterPlayer.is_on_wall():
+		CharacterPlayer.last_wall_normal = CharacterPlayer.get_wall_normal()  # Get direction of the wall
+		print("Wall Bounce Normal: ", CharacterPlayer.last_wall_normal)  # Debugging
 
-	# Remove vertical component of the normal (prevents bouncing upwards)
-	CharacterPlayer.last_wall_normal.y = 0
-	CharacterPlayer.last_wall_normal = CharacterPlayer.last_wall_normal.normalized()
+		# Remove vertical component of the normal (prevents bouncing upwards)
+		CharacterPlayer.last_wall_normal.y = 0
+		CharacterPlayer.last_wall_normal = CharacterPlayer.last_wall_normal.normalized()
 
-	# Apply bounce force in the wall's normal direction + some forward movement
-	CharacterPlayer.velocity = (CharacterPlayer.last_wall_normal * CharacterPlayer.wall_bounce_multiplier * CharacterPlayer.SPEED) + Vector3(0, CharacterPlayer.wall_boost, 0)
-	CharacterPlayer.is_wall_bouncing = true
-	
+		# Apply bounce force in the wall's normal direction + some forward movement
+		CharacterPlayer.velocity = (CharacterPlayer.last_wall_normal * CharacterPlayer.wall_bounce_multiplier * CharacterPlayer.SPEED) + Vector3(0, CharacterPlayer.wall_boost, 0)
+		CharacterPlayer.is_wall_bouncing = true
+		
 
-	
+		
